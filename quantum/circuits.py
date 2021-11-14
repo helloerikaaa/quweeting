f6628b9f30eca5465438a31e3a4ed38f6d2ef610
Erika <esanchezf1201@alumno.ipn.mx>
2021-11-13 21:03:41 -0600

    ADD: Quantum circuits for diagrams of sentences

---
+++ quantum/circuits.py
@@ -0,0 +1,22 @@
+from discopy import Dim
+
+from lambeq.tensor import SpiderAnsatz
+from lambeq.core.types import AtomicType
+
+
+class Circuit:
+
+    ansatz = SpiderAnsatz({AtomicType.NOUN: Dim(2),
+                           AtomicType.SENTENCE: Dim(2)})
+
+    def create_circuit(self, diagram):
+        circuits = [self.ansatz(d) for d in diagram]
+
+        return circuits
+
+    def draw_circuits(self, circuits):
+        for i in enumerate(circuits):
+            self.draw_circuit(circuits, i)
+
+    def draw_circuit(self, circuits, i):
+        circuits[i].draw()
