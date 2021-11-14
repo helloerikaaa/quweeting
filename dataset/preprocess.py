1c68c5dc6cca4b15c751b1784e9fba37d212dded
Erika <esanchezf1201@alumno.ipn.mx>
2021-11-13 23:06:55 -0600

    MODIFY: Line to download stopwords using nltk

--- dataset/preprocess.py
+++ dataset/preprocess.py
@@ -2,6 +2,8 @@ import re
 import string
 import unicodedata
 
+import nltk
+#nltk.download('stopwords')
 from nltk.corpus import stopwords
 
 STOPWORDS = stopwords.words('english')
--- quantum/diagrams.py
+++ quantum/diagrams.py
@@ -4,7 +4,7 @@ from lambeq.ccg2discocat import DepCCGParser
 class Diagram:
     reader = DepCCGParser(possible_root_cats=['S[dcl]'])
 
-    def _create_diagram(self, dataset):
+    def create_diagram(self, dataset):
         diagrams = self.reader.sentences2diagrams(dataset)
 
         return diagrams
